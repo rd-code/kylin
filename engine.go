acdee679d01d934bb585ec1448c4a131f96a68a0
lijunbao_dxm <lijunbao@duxiaoman.com>
2021-09-09 11:55:40 +0800

    添加日志打印信息

--- engine.go
+++ engine.go
@@ -1,18 +1,26 @@
 package kylin
 
 import (
-	"github.com/rd-code/kylin/route"
 	"net/http"
+
+	"github.com/rd-code/kylin/log"
+	"github.com/rd-code/kylin/route"
 )
 
 type Engine struct {
 	router route.Router
+	logger log.Logger
 }
 
 func NewEngine() *Engine {
 	return &Engine{
 		router: route.NewRouter(),
+		logger: log.Default,
 	}
+}
+
+func (e *Engine) SetLogger(log log.Logger) {
+	e.logger = log
 }
 
 func (e *Engine) Get(path string, f func(ctx *Context)) {
@@ -38,10 +46,12 @@ func (e *Engine) Handle(path string, f func(ctx *Context)) {
 func (e *Engine) Group(path string) *Engine {
 	res := NewEngine()
 	res.router = e.router.Group(path)
+	res.logger = e.logger
 	return res
 }
 
 func (e *Engine) ServeHTTP(response http.ResponseWriter, request *http.Request) {
+	e.logger.Info("receive request, method:%s, path:%s", request.Method, request.URL.Path)
 	handler := e.router.GetHandler(request.Method, request.URL.Path)
 	if handler == nil {
 		response.WriteHeader(http.StatusNotFound)
